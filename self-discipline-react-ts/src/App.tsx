9426ae2f9bac31f3f5a20eb6440ee01578274dad
coldrain-f <intmain__@naver.com>
2023-08-02 20:18:34 +0900

    Chore: 프로젝트 자동 생성 샘플 코드 제거 [#1]

--- self-discipline-react-ts/src/App.tsx
+++ self-discipline-react-ts/src/App.tsx
@@ -1,31 +1,22 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function App() {
+  const label: string = "Self discipline";
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Vite.js example in TypeScript
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ flexGrow: 1 }}
+          color="primary"
+        >
+          {label.toUpperCase()}
         </Typography>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
