89522c9f020c0425f6a6f44aef6ee0a79e3dc00c
coldrain-f <intmain__@naver.com>
2023-08-04 19:40:39 +0900

    Feat: AppBar 기본 레이아웃 생성 [#2]

--- self-discipline-react-ts/src/App.tsx
+++ self-discipline-react-ts/src/App.tsx
@@ -2,22 +2,13 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import SelfDisciplineAppBar from "./components/SelfDisciplineAppBar";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  const label: string = "Self discipline";
-
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{ flexGrow: 1 }}
-          color="primary"
-        >
-          {label.toUpperCase()}
-        </Typography>
-      </Box>
+    <Container maxWidth="xl">
+      <SelfDisciplineAppBar />
     </Container>
   );
 }
