2f23cabebaed5122533b676e501d91c43c87c9e1
coldrain-f <intmain__@naver.com>
2023-08-05 18:18:04 +0900

    Feat: 테이블 헤더 한글로 변경 [#4]

--- self-discipline-react-ts/src/components/planner/SelfDisciplinePlannerTodoList.tsx
+++ self-discipline-react-ts/src/components/planner/SelfDisciplinePlannerTodoList.tsx
@@ -29,7 +29,7 @@ export default function SelfDisciplinePlannerTodoList() {
       >
         <InputBase
           sx={{ ml: 1, flex: 1 }}
-          placeholder="Please enter your to-do list."
+          placeholder="할 일을 입력해주세요."
         />
         <IconButton sx={{ p: "10px" }}>
           <AddIcon />
@@ -39,10 +39,10 @@ export default function SelfDisciplinePlannerTodoList() {
         <Table sx={{}}>
           <TableHead>
             <TableRow>
-              <TableCell align="left"></TableCell>
-              <TableCell align="left">Title</TableCell>
-              <TableCell align="left">Status</TableCell>
-              <TableCell align="left">Priority</TableCell>
+              <TableCell align="left">선택</TableCell>
+              <TableCell align="left">제목</TableCell>
+              <TableCell align="left">상태</TableCell>
+              <TableCell align="left">우선 순위</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -54,10 +54,10 @@ export default function SelfDisciplinePlannerTodoList() {
                 일본어 단어 10개 암기하기
               </TableCell>
               <TableCell align="left" sx={{ width: "15%" }}>
-                B
+                미완료
               </TableCell>
               <TableCell align="left" sx={{ width: "15%" }}>
-                C
+                보통
               </TableCell>
             </TableRow>
           </TableBody>
