27362e83654ecf6520cce64200282af3532828c4
coldrain-f <intmain__@naver.com>
2023-08-05 18:08:18 +0900

    Feat: 플래너 todo list 기본 틀 생성 [#4]

--- self-discipline-react-ts/src/components/planner/SelfDisciplinePlanner.tsx
+++ self-discipline-react-ts/src/components/planner/SelfDisciplinePlanner.tsx
@@ -58,7 +58,54 @@ export default function SelfDisciplinePlanner() {
           </List>
         </Box>
       </Grid>
-      <Grid xs={5}></Grid>
+      <Grid xs={5}>
+        <Paper
+          component="form"
+          sx={{
+            my: 2,
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+          }}
+          variant="outlined"
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Please enter your to-do list."
+          />
+          <IconButton sx={{ p: "10px" }}>
+            <AddIcon />
+          </IconButton>
+        </Paper>
+        <TableContainer component={Paper}>
+          <Table sx={{}}>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left"></TableCell>
+                <TableCell align="left">Title</TableCell>
+                <TableCell align="left">Status</TableCell>
+                <TableCell align="left">Priority</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align="left" sx={{ width: "10%" }}>
+                  <Checkbox />
+                </TableCell>
+                <TableCell align="left" sx={{ width: "60%" }}>
+                  일본어 단어 10개 암기하기
+                </TableCell>
+                <TableCell align="left" sx={{ width: "15%" }}>
+                  B
+                </TableCell>
+                <TableCell align="left" sx={{ width: "15%" }}>
+                  C
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
       <Grid xs={5}></Grid>
     </Grid>
   );
